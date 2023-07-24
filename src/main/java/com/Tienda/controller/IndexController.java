ec0a148987d9ba368b1fa36e2dfa0fbe537fd55a
Chappy0710 <111452188+Chappy0710@users.noreply.github.com>
2023-07-23 19:39:49 -0600

    Commit

--- src/main/java/com/Tienda/controller/IndexController.java
+++ src/main/java/com/Tienda/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.Tienda.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     
     @RequestMapping("/")
-    public String page(Model model) {
+    public String page(Model model, HttpSession session) {
+        
+        String imagen = (String)session.getAttribute("usuarioImagen");
+        model.addAttribute("avatar", imagen);
         return "index";
     }
     
